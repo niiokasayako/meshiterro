8d174e0cb37a726cef8c428776e0cd5573607164
niiokasayako <107908957+niiokasayako@users.noreply.github.com>
2022-07-07 01:58:36 +0000

    [Add]4-13 削除機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,19 @@
 class ApplicationController < ActionController::Base
+  
   before_action :configure_pemitted_parameters, if: :devise_controller?
   
-  def after_sign_in_path_for(resource)
-    post_images_path
-  end
+   def after_sign_in_path_for(resource)
+     post_images_path
+   end
   
-  def after_sign_out_path_for(resourse)
-    about_path
-  end
+   def after_sign_out_path_for(resourse)
+     about_path
+   end
   
-  protected
+    protected
   
-  def configure_pemitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+    def configure_pemitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
 end
